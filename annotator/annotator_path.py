bb6a16569bc24bcc0128dee06eb6ee3b823b1d98
wangdongming <wangdongming@hifive.ai>
2023-04-20 20:27:49 +0800

    [feat](annotator_path): 修改controlnet模型路径

--- annotator/annotator_path.py
+++ annotator/annotator_path.py
@@ -4,6 +4,8 @@ from modules import shared
 models_path = shared.opts.data.get('control_net_modules_path', None)
 if not models_path:
     models_path = getattr(shared.cmd_opts, 'controlnet_annotator_models_path', None)
+if os.path.isdir('models/ControlNet'):
+    models_path = 'models/ControlNet'
 if not models_path:
     models_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads')
 
